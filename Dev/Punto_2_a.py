a3ce4019f03da376f5bb68d96f60ee6354c33393
Nacho <ignacio.boada@outlook.com>
2018-05-01 20:52:33 -0300

    Corregida la F y agregado el punto 2

--- Dev/Punto_2_a.py
+++ Dev/Punto_2_a.py
@@ -7,7 +7,7 @@ import math #Para el logaritmo
     #Declaro variables del problema
 NP = 99411
 g = 9.81
-Lo =  (2 * 100000) / NP
+Lo =  2 * 100000 / NP
 Lo = round(Lo, 3)
 K = 10
 Mo = 100000 / NP
@@ -15,9 +15,9 @@ Mo = round(Mo, 3)
 a = 1
 
 #Esta es la funcion a la que se le deben hallar las raices
-''' Fy = -2 * K * y * (1 - (Lo / (y**2 + a**2))**(1/2)) - m * g '''
+''' Fy = -2 * k * y * (1 - (Lo / (y**2 + a**2))**(1/2)) - m * g '''
 def F(y, m = 0):
-    return (-2 * K * y * (1 - (Lo / (y**2 + a**2)**(1/2))) - m * g)
+    return (-2 * K * y * (1 - (Lo / (y**2 + a**2))**(1/2)) - m * g)
 
 def cte_asintotica_del_error(r_kmas1, r_k, r_kmenos1, p):
     return (abs((r_kmas1 - r_k)) / abs((r_k - r_kmenos1))**p)
@@ -26,29 +26,30 @@ def orden_de_convergencia(r_kmas1, r_k, r_kmenos1, r_kmenos2):
     return (math.log( abs((r_kmas1 - r_k))/ abs((r_k - r_kmenos1)) ) / math.log( abs((r_k - r_kmenos1))/ abs((r_kmenos1 - r_kmenos2)) ))
 
 
-''' Punto 1 - Mo = 0 '''
+''' Punto 2 - M = 0.3 * Mo '''
 
 #a) Regula-Falsi:
+M = 0.3 * Mo
 a_k = 0.1
-b_k = 2
+b_k = 19
 iteraciones = 90
 r_kmenos2 = 0
 r_kmenos1 = 0
 r_k = 0 #Los inicializo para poder usarlo en cuando k>0, sino da error.
 print "k\ta_k\tb_k\tF(a_k)\tF(b_k)\tr_kmas1\tDELTAr (r_k - r_k-1)\tDELTAr/r_k\tlambda\tp"
 for k in range(iteraciones):
-    r_kmas1 = a_k - ((b_k - a_k) / (F(b_k) - F(a_k))) * F(a_k)
+    r_kmas1 = a_k - ((b_k - a_k) / (F(b_k, M) - F(a_k, M))) * F(a_k, M)
     if k == 1:
         print "" #Para que no imprima todo junto porque cuando k=0 los delta r no existen.
-    print k, '\t', a_k, '\t', b_k, '\t', F(a_k), '\t', F(b_k), '\t', r_kmas1,
-    if (F(r_kmas1) == 0):
+    print k, '\t', a_k, '\t', b_k, '\t', F(a_k, M), '\t', F(b_k, M), '\t', r_kmas1,
+    if (F(r_kmas1, M) == 0):
         raiz = r_kmas1
         delta_r_kmas1 = r_kmas1 - r_k
         err_rel = abs(delta_r_kmas1 / r_kmas1)
         print '\t', delta_r_kmas1, '\t', err_rel
         print "\nRAIZ HALLADA: ", raiz
         break
-    elif (F(r_kmas1) * F(a_k) > 0):
+    elif (F(r_kmas1, M) * F(a_k, M) > 0):
         a_k = r_kmas1
     else:
         b_k = r_kmas1
