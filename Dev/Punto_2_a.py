9efa62037a13b48f633ba2aa22321d644fea8d01
Nacho <ignacio.boada@outlook.com>
2018-05-01 22:22:28 -0300

    Punto 2 a terminado

--- Dev/Punto_2_a.py
+++ Dev/Punto_2_a.py
@@ -26,12 +26,16 @@ def orden_de_convergencia(r_kmas1, r_k, r_kmenos1, r_kmenos2):
     return (math.log( abs((r_kmas1 - r_k))/ abs((r_k - r_kmenos1)) ) / math.log( abs((r_k - r_kmenos1))/ abs((r_kmenos1 - r_kmenos2)) ))
 
 
+
 ''' Punto 2 - M = 0.3 * Mo '''
 
 #a) Regula-Falsi:
+
+#RAIZ 1:
+print "RAIZ 1:"
 M = 0.3 * Mo
-a_k = -5
-b_k = -1
+a_k = 0.1
+b_k = 1
 iteraciones = 90
 r_kmenos2 = 0
 r_kmenos1 = 0
@@ -42,7 +46,7 @@ for k in range(iteraciones):
     if k == 1:
         print "" #Para que no imprima todo junto porque cuando k=0 los delta r no existen.
     print k, '\t', a_k, '\t', b_k, '\t', F(a_k, M), '\t', F(b_k, M), '\t', r_kmas1,
-    if (F(r_kmas1, M) == 0):
+    if (F(r_kmas1, M) == 0 or (F(r_kmas1, M) < 10**(-15) and F(r_kmas1, M) > -10**(-15))):
         raiz = r_kmas1
         delta_r_kmas1 = r_kmas1 - r_k
         err_rel = abs(delta_r_kmas1 / r_kmas1)
@@ -67,6 +71,75 @@ for k in range(iteraciones):
     r_kmenos1 = r_k
     r_k = r_kmas1
     
-    
+#RAIZ 2:
+print "\nRAIZ 2:"
+a_k = 1
+b_k = 2
+print "k\ta_k\tb_k\tF(a_k)\tF(b_k)\tr_kmas1\tDELTAr (r_k - r_k-1)\tDELTAr/r_k\tlambda\tp"
+for k in range(iteraciones):
+    r_kmas1 = a_k - ((b_k - a_k) / (F(b_k, M) - F(a_k, M))) * F(a_k, M)
+    if k == 1:
+        print "" #Para que no imprima todo junto porque cuando k=0 los delta r no existen.
+    print k, '\t', a_k, '\t', b_k, '\t', F(a_k, M), '\t', F(b_k, M), '\t', r_kmas1,
+    if (F(r_kmas1, M) == 0 or (F(r_kmas1, M) < 10**(-14) and F(r_kmas1, M) > -10**(-14))): #Con 10**-15 da error
+        raiz = r_kmas1
+        delta_r_kmas1 = r_kmas1 - r_k
+        err_rel = abs(delta_r_kmas1 / r_kmas1)
+        print '\t', delta_r_kmas1, '\t', err_rel
+        print "\nRAIZ HALLADA: ", raiz
+        break
+    elif (F(r_kmas1, M) * F(a_k, M) > 0):
+        a_k = r_kmas1
+    else:
+        b_k = r_kmas1
+    if k > 0:
+        delta_r_kmas1 = r_kmas1 - r_k
+        err_rel = abs(delta_r_kmas1 / r_kmas1)
+        print '\t', delta_r_kmas1, '\t', err_rel,
+        if k <= 2:
+            print "" #Para que no imprima todo junto mientras no se pueden calcular p y lambda
+        if k > 2: #Necesito 4 valores de r       
+            p = orden_de_convergencia(r_kmas1, r_k, r_kmenos1, r_kmenos2)
+            lamda = cte_asintotica_del_error(r_kmas1, r_k, r_kmenos1, p)
+            print '\t', lamda, '\t', p
+    r_kmenos2 = r_kmenos1
+    r_kmenos1 = r_k
+    r_k = r_kmas1
+
+#RAIZ 3:
+print "\nRAIZ 3:"
+a_k = -1
+b_k = -3
+print "k\ta_k\tb_k\tF(a_k)\tF(b_k)\tr_kmas1\tDELTAr (r_k - r_k-1)\tDELTAr/r_k\tlambda\tp"
+for k in range(iteraciones):
+    r_kmas1 = a_k - ((b_k - a_k) / (F(b_k, M) - F(a_k, M))) * F(a_k, M)
+    if k == 1:
+        print "" #Para que no imprima todo junto porque cuando k=0 los delta r no existen.
+    print k, '\t', a_k, '\t', b_k, '\t', F(a_k, M), '\t', F(b_k, M), '\t', r_kmas1,
+    if (F(r_kmas1, M) == 0 or (F(r_kmas1, M) < 10**(-14) and F(r_kmas1, M) > -10**(-14))): #Con 10**-15 da error.
+        raiz = r_kmas1
+        delta_r_kmas1 = r_kmas1 - r_k
+        err_rel = abs(delta_r_kmas1 / r_kmas1)
+        print '\t', delta_r_kmas1, '\t', err_rel
+        print "\nRAIZ HALLADA: ", raiz
+        break
+    elif (F(r_kmas1, M) * F(a_k, M) > 0):
+        a_k = r_kmas1
+    else:
+        b_k = r_kmas1
+    if k > 0:
+        delta_r_kmas1 = r_kmas1 - r_k
+        err_rel = abs(delta_r_kmas1 / r_kmas1)
+        print '\t', delta_r_kmas1, '\t', err_rel,
+        if k <= 2:
+            print "" #Para que no imprima todo junto mientras no se pueden calcular p y lambda
+        if k > 2: #Necesito 4 valores de r       
+            p = orden_de_convergencia(r_kmas1, r_k, r_kmenos1, r_kmenos2)
+            lamda = cte_asintotica_del_error(r_kmas1, r_k, r_kmenos1, p)
+            print '\t', lamda, '\t', p
+    r_kmenos2 = r_kmenos1
+    r_kmenos1 = r_k
+    r_k = r_kmas1
+
     
 
