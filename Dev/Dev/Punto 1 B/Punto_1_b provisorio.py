88ad901739f5db7a77e022cff948613d386d6469
FrancoR <33163113+francoriboratig@users.noreply.github.com>
2018-05-02 00:23:46 -0300

    Rename Dev/Punto_1_b provisorio.py to Dev/Dev/Punto 1 B/Punto_1_b provisorio.py

--- Dev/Dev/Punto 1 B/Punto_1_b provisorio.py
+++ Dev/Dev/Punto 1 B/Punto_1_b provisorio.py
@@ -21,7 +21,7 @@ a = 1
 def F(y):
     return math.sqrt(y)
 
-#Próximo, se define un punto perteneciente al intervalo que cumple con los requisitos
+#PrÃ³ximo, se define un punto perteneciente al intervalo que cumple con los requisitos
 #xseed por la variable x y por ser la semilla del problema :)
 xseed = 0.5
 
